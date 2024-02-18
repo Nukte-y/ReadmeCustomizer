2f23a72b4fdc28e4531214e65c28dd85f14adc21
nukte-y <nukteu@gmail.com>
2024-02-18 23:21:18 +0000

    added questions

--- index.js
+++ index.js
@@ -5,16 +5,64 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+{
+    type: "input",
+    name: "title",
+    message: "What is your project's title?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project:"
+},
+{
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT","Unlicence","GNU","BSD","Apache 2.0"]
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Please enter installation instructions:"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Please enter information for usage section:"
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "Please enter contribution guidelines:"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Please enter test guidelines:"
+},
+{
+    type: "input",
+    name: "gitHub",
+    message: "Please enter GitHub username:"
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter email adress:"
+},
 ];
-
 // function to write README file
 function writeToFile(fileName, data) {
+
+
+
+
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answers)=> writeToFile(answers));
 }
 
 // function call to initialize program
