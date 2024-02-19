624842a0ec26e05fd7f79a8a7831426f7a069c88
nukte-y <nukteu@gmail.com>
2024-02-19 17:28:25 +0000

    added licences links and badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,24 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}\n\n` + 
+  let licenseBadge = '';
+  let licenseURL = '';
+
+  if (data.license === "MIT") {
+      licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n";
+      licenseURL = "https://opensource.org/licenses/MIT";
+  }  else if (data.license === "GNU") {
+      licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n";
+      licenseURL = "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (data.license === "BSD") {
+      licenseBadge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n\n";
+      licenseURL = "https://opensource.org/licenses/BSD-3-Clause";
+  } else if (data.license === "Apache 2.0") {
+      licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n";
+      licenseURL = "https://opensource.org/licenses/Apache-2.0";
+  };
+
+  return `# ${data.title}\n\n` +
+         `${licenseBadge} ${licenseURL} \n\n` +
          `## Table of Contents\n` +
          `- [Description](#description)\n` +
          `- [Installation](#installation)\n` +
