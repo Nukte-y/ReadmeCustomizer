fc6871931ab2e4743f7fbe8065494c25b4fa3f70
nukte-y <nukteu@gmail.com>
2024-02-19 12:06:38 +0000

    added table of content reference links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,17 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}\n\n` + 
+         `## Table of Contents\n` +
+         `- [Description](#description)\n` +
+         `- [Installation](#installation)\n` +
+         `- [Usage](#usage)\n` +
+         `- [Contributing](#contributing)\n\n` +
+         `- [Tests](#tests)\n\n` +
          `## Description\n\n${data.description}\n\n` +
          `## Installation\n\n${data.installation}\n\n` +
          `## Usage\n\n${data.usage}\n\n` +
          `## Contributing\n\n${data.contributing}\n\n` +
-         `## Tests\n\n${data.test}\n\n` 
+         `## Tests\n\n${data.tests}\n\n` 
 }
 
 module.exports = generateMarkdown;
